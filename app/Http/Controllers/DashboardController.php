94819c1e7321d8ec7cddb9c8d67571faf12398a8
VheeeeeeeeeJhay <vheebhee7@gmail.com>
2025-03-14 15:51:32 +0800

    error handling nlng din

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,7 @@ use App\Models\Incident;
 use App\Models\TypeOfAssistance;
 use App\Models\Report;
 use Illuminate\Support\Facades\DB;
+use Exception;
 
 class DashboardController extends Controller
 {
@@ -19,9 +20,33 @@ class DashboardController extends Controller
         $report = Report::all();
 
         return response()->json([
-                'incidents' => $incidents, 
-                'assistance' => $assistance,
-                'report' => $report
+            'incidents' => $incidents, 
+            'assistance' => $assistance,
+            'report' => $report
+        ], 200);
+    }
+
+    public function recent()
+    {
+        try {
+        $recent = Report::with(['source:id,sources', 'incident:id,type', 'actions:id,actions', 'assistance:id,assistance', 'barangay:id,name,longitude,latitude'])->latest()->take(5)->get();
+        return response()->json([
+            'recent' => $recent
+        ], 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Something went wrong'], 500);
+        }
+    }
+
+    public function total_reports()
+    {
+        try {
+            $total = Report::count();
+            return response()->json([
+                'total' => $total
             ], 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Something went wrong'], 500);
+        }
     }
 }
