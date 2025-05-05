007eeb7d6fd066c3df2b88d597a8d1860e98989d
oliverkeanpalgue <106715785+oliverkeanpalgue@users.noreply.github.com>
2025-05-05 11:07:04 +0800

    feat: outbound and inbound models added fillable

--- app/Models/Inbound.php
+++ app/Models/Inbound.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inbound extends Model
 {
-    //
+    protected $table = 'inbound';
+
+    protected $fillable = [
+        'road_id',
+        'status_traffic_id',
+    ];
+
+    public function road()
+    {
+        return $this->belongsTo(Road::class);
+    }
+
+    public function statusTraffic()
+    {
+        return $this->belongsTo(StatusTraffic::class);
+    }
+
+
 }
