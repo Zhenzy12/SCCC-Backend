2c2c80644f2c5b8e56eb445f7bcd0f1c3b8dbb0f
oliverkeanpalgue <106715785+oliverkeanpalgue@users.noreply.github.com>
2025-05-06 10:48:10 +0800

    feat: added road controller and api route

--- app/Http/Controllers/RoadController.php
+++ app/Http/Controllers/RoadController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Road;
 
 class RoadController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+        try {
+            $roads = Road::all();
+
+            return response()->json([
+                'roads' => $roads,
+            ], 200);
+
+        } catch (Exception $e) {
+
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+
+        }
+    }
 }
