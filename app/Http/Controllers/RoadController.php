87a74bd4b2e986ae55cc537727713d1500a620a0
oliverkeanpalgue <106715785+oliverkeanpalgue@users.noreply.github.com>
2025-05-07 10:44:44 +0800

    feat: added inbound and outbound for controller

--- app/Http/Controllers/RoadController.php
+++ app/Http/Controllers/RoadController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Road;
+use App\Models\Inbound;
+use App\Models\Outbound;
 
 class RoadController extends Controller
 {
+
     public function index()
     {
         //
         try {
             $roads = Road::all();
+            $inbounds = Inbound::all();
+            $outbounds = Outbound::all();
+            foreach ($roads as $road) {
+                $road->inbound = $inbounds->where('road_id', $road->id)->first();
+                $road->outbound = $outbounds->where('road_id', $road->id)->first();
+            }
 
             return response()->json([
                 'roads' => $roads,
